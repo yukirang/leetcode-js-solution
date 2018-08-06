41b55f3edb0560e040c5cdea0798a69a047ccea3
yukirang <rangk@163.com>
2018-08-06 16:45:23 +0800

    151

---
+++ 151. Reverse Words in a String.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} str
+ * @returns {string}
+ */
+var reverseWords = function(str) {
+    
+    return str.trim().replace(/\s+/g," ").split(" ").reverse().join(' ').toString();
+};

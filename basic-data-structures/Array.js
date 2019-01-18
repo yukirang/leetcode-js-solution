5418a0c6bf5be86e7c973788559cf587344147a5
yukirang <rangk@163.com>
2019-01-18 13:01:27 -0800

    array construction

--- basic-data-structures/Array.js
+++ basic-data-structures/Array.js
@@ -16,3 +16,55 @@ strings.unshift('x'); //O(n)
 strings.splice(2, 1, 'alien'); //O(n)
 
 console.log(strings); 
+
+
+//construction:class
+
+class MyArray{
+	constructor(){
+		this.length = 0;
+		this.data = {};
+	}
+
+	get(index){
+		return this.data[index]
+	}
+
+	push(item){
+		this.data[this.length] = item;
+		this.length++;
+		return this.length;
+	}
+
+	pop(){
+		const lastItem = this.data[this.length-1];
+		delete this.data[this.length-1];
+		this.length--;
+		return lastItem;
+	}
+
+	delete(index){
+		const item = this.data[index];
+		this.shiftItems(index);
+		return item;
+	}
+
+	shiftItems(index){
+		for (let i = index; i < this.length - 1; i++){
+			this.data[i] = this.data[i+1];
+		}
+		delete this.data[this.length-1];
+		this.length--;
+	}
+
+}
+
+const newArray = new MyArray();
+newArray.push('hello');
+newArray.push('world');
+newArray.push('!');
+newArray.pop();
+newArray.delete(0)
+
+console.log(newArray.get(0));
+

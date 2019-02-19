390121bd1b59c297eb37db84ad670c1465ef1b14
yukirang <rangk@163.com>
2019-02-19 14:22:08 -0800

    update

--- Array&String/344. Reverse String.js
+++ Array&String/344. Reverse String.js
@@ -18,3 +18,13 @@ var reverseString = function(s) {
 	This is my solution towards the "two pointers" method in the "explore" tutorial;
 	However, one better way might be just start from the last and end at the first.
 */
+
+/*Update*/
+
+//JavaScript built-in
+function reverse2(str){
+    return str.split('').reverse.join('');
+}
+
+//ES6
+const reverse3 = str => [...str].reverse.join('');
